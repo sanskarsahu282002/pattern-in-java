12c0cf441d55cbd5d8fbd9e169cfa1f4299dff08
sanskarsahu282002 <116749721+sanskarsahu282002@users.noreply.github.com>
2023-04-05 04:42:37 +0530

    Update halfpyramid.java

--- halfpyramid.java
+++ halfpyramid.java
@@ -6,7 +6,8 @@ public class halfpyramid {
         {
             int i,j;
             for( i = 1;i<=5;i++){
-                for (j=1;j<=6-i;j++){
+                for (j=1;j<=6-i;j++)
+                {
                 System.out.print('*');
                 }
                 System.out.println();
